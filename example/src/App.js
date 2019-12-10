227d8ce5b58a0ef74b28593ec4d93a960c612ecc
Chintan Prajapati <chintan9@student.fdu.edu>
2019-12-10 16:47:59 +0000

    Initial Commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'plyr-react'
+import PlyrComponent from 'plyr-react'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <PlyrComponent/>
       </div>
     )
   }
