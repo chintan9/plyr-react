a54eefeb6272e307c95cdc9489ccf24b4a30a5fc
Chintan Prajapati <chintan9@student.fdu.edu>
2019-12-12 00:04:45 +0000

    update

--- rollup.config.js
+++ rollup.config.js
@@ -7,33 +7,37 @@ import { terser } from 'rollup-plugin-terser';
 
 
 export default {
-    input: './src/index.js',
-    output: {
-        file: './build/bundle.min.js',
-        format: 'iife',
-        name: 'bundle',
-        globals: {
-            'plyr': 'plyr',
-        }
-    },
-     plugins: [
-         babel({
-        exclude: 'node_modules/**',
-        babelrc: false,
-        presets: [
-          [
-            '@babel/env',
-            {
-              modules: false,
-              useBuiltIns: "usage",
-              targets: 'maintained node versions'
+  input: './src/index.js',
+  output: {
+    file: './build/bundle.min.js',
+    format: 'iife',
+    name: 'bundle',
+  },
+  plugins: [
+    babel({
+      exclude: [
+        'node_modules/**',
+      ],
+      babelrc: false,
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            "useBuiltIns": "false", // "usage" | "entry" | false, defaults to false.
+            "corejs": "3.0.0",
+            "targets": {
+              "esmodules": true,
+              "ie": "11"
             }
-          ]
+          }
         ]
-      }),
-        resolve(),
-        commonjs(),
-        uglify(),
-        terser()
-    ]
+      ]
+    }),
+    resolve(),
+    commonjs({
+      exclude: 'src/**',
+    }),
+    uglify(),
+    terser()
+  ]
 }
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import React from 'react'
 import PropTypes from 'prop-types'
 import plyr from 'plyr'
