61b2a1652e4ceecd61b4520e9b654c78ae1b4ac8
Restyled.io <commits@restyled.io>
2019-12-19 21:17:49 +0000

    Restyled by prettier

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
-import './style.css';
+import "./style.css";
 
-import React, {Component} from 'react';
-import {render} from 'react-dom';
+import React, { Component } from "react";
+import { render } from "react-dom";
 
-import PlyrComponent from './PlyrComponent';
+import PlyrComponent from "./PlyrComponent";
 
 class App extends Component {
-  constructor() { super(); }
+  constructor() {
+    super();
+  }
 
   render() {
-    return (<div className = "plyrwrapper"><PlyrComponent />
-            <p>The example of react plyr</p>
-      </div>);
+    return (
+      <div className="plyrwrapper">
+        <PlyrComponent />
+        <p>The example of react plyr</p>
+      </div>
+    );
   }
 }
 
-render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
