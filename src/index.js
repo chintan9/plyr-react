e748bfd64920ef83eb84f4bb1994fbb4c29bae8e
Restyled.io <commits@restyled.io>
2019-12-19 21:17:47 +0000

    Restyled by clang-format

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
-import PlyrComponent from './PlyrComponent';
 import './style.css';
 
+import React, {Component} from 'react';
+import {render} from 'react-dom';
+
+import PlyrComponent from './PlyrComponent';
+
 class App extends Component {
-  constructor() {
-    super();
-  }
+  constructor() { super(); }
 
   render() {
-    return (
-      <div className ="plyrwrapper">
-        <PlyrComponent/>
-         <p>
-          The example of react plyr
-        </p>
-      </div>
-    );
+    return (<div className = "plyrwrapper"><PlyrComponent />
+            <p>The example of react plyr</p>
+      </div>);
   }
 }
 
