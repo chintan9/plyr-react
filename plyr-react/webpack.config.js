899ea571a759bde751099b71cad745d68ace40d0
Chintan Prajapati <chintan9@student.fdu.edu>
2020-01-16 21:11:02 +0000

    update

--- plyr-react/webpack.config.js
+++ plyr-react/webpack.config.js
@@ -6,6 +6,10 @@ const htmlWebpackPlugin = new HtmlWebpackPlugin({
 });
 module.exports = {
     entry: path.join(__dirname, "examples/src/index.js"),
+    output: {
+        path: path.join(__dirname, "examples/dist"),
+        filename: "bundle.js"
+    },
     module: {
         rules: [
             {
