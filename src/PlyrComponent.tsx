546b06f6986392ea58f34ee36dc60518b35e26c4
Takeshi Iwana <tak@prismify.co>
2020-07-14 10:53:39 -0600

    Disable eslint for prop type

--- src/PlyrComponent.tsx
+++ src/PlyrComponent.tsx
@@ -85,6 +85,7 @@ Plyr.defaultProps = {
 
 Plyr.propTypes = {
   options: PropTypes.object,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   source: PropTypes.oneOfType([object, null as any, undefined]),
 }
 
