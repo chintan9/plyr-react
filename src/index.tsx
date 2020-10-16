eaea4902211aa7c47d2abc300be2f12145dbfc2f
Takeshi Iwana <tak@prismify.co>
2020-10-16 12:02:51 -0600

    Don't destroy plyr instance on rerender

--- src/index.tsx
+++ src/index.tsx
@@ -38,8 +38,6 @@ export const Plyr = React.forwardRef<HTMLPlyrVideoElement, PlyrProps>(
       if (innerRef.current && source) {
         innerRef.current.plyr.source = source
       }
-
-      return () => innerRef?.current?.plyr?.destroy()
     }, [ref, source])
 
     return (
