35eee7d38dad654d2a1720be9c2f39112db4a814
Chintan Prajapati <chintan9@student.fdu.edu>
2021-02-10 23:33:18 +0000

    now using blank video as default

--- src/index.tsx
+++ src/index.tsx
@@ -101,13 +101,13 @@ Plyr.defaultProps = {
     sources: [
       {
         src:
-          'https://rawcdn.githack.com/chintan9/Big-Buck-Bunny/e577fdbb23064bdd8ac4cecf13db86eef5720565/BigBuckBunny720p30s.mp4',
+          'https://cdn.plyr.io/static/blank.mp4',
         type: 'video/mp4',
         size: 720,
       },
       {
         src:
-          'https://rawcdn.githack.com/chintan9/Big-Buck-Bunny/e577fdbb23064bdd8ac4cecf13db86eef5720565/BigBuckBunny1080p30s.mp4',
+          'https://cdn.plyr.io/static/blank.mp4',
         type: 'video/mp4',
         size: 1080,
       },
