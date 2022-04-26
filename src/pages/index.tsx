60316b0964af044abc05e8e2b88f252f08bec2e2
Chintan Prajapati <chintan9@student.fdu.edu>
2022-04-26 06:22:26 +0000

    wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,7 +17,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Plyr-react Tutorial - 1min ⏱️
           </Link>
         </div>
       </div>
