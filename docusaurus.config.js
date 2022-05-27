24e4de8aca771d34238253493b809743d98ed47e
Chintan Prajapati <chintan9@users.noreply.github.com>
2022-05-27 21:21:18 +0000

    add genrator

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,101 +1,101 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Plyr-react',
-  tagline: 'Plyr-react-docs',
-  url: 'https://chintan9.github.io/',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  title: "Plyr-react",
+  tagline: "Plyr-react-docs",
+  url: "https://chintan9.github.io/",
+  baseUrl: "/plyr-react/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   trailingSlash: true,
-  favicon: 'img/favicon.ico',
-  organizationName: 'chintan9', // Usually your GitHub org/user name.
-  projectName: 'plyr-react', // Usually your repo name.
+  favicon: "img/favicon.ico",
+  organizationName: "chintan9", // Usually your GitHub org/user name.
+  projectName: "plyr-react", // Usually your repo name.
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
-          editUrl: 'https://github.com/chintan9/plyr-react/tree/docs',
+          editUrl: "https://github.com/chintan9/plyr-react/tree/docs"
         },
 
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      })
+    ]
   ],
 
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'Plyr-react',
+        title: "Plyr-react",
         logo: {
-          alt: 'Plyr-react Logo',
-          src: 'img/logo.svg',
+          alt: "Plyr-react Logo",
+          src: "img/logo.svg"
         },
         items: [
           {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
+            type: "doc",
+            docId: "intro",
+            position: "left",
+            label: "Tutorial"
           },
           {
-            href: 'https://github.com/chintan9/plyr-react',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
+            href: "https://github.com/chintan9/plyr-react",
+            label: "GitHub",
+            position: "right"
+          }
+        ]
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
+                label: "Tutorial",
+                to: "/docs/intro"
+              }
+            ]
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/chintan9/plyr-react',
-              },
-            ],
+                label: "GitHub",
+                href: "https://github.com/chintan9/plyr-react"
+              }
+            ]
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/chintan9/plyr-react',
-              },
-            ],
-          },
+                label: "GitHub",
+                href: "https://github.com/chintan9/plyr-react"
+              }
+            ]
+          }
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} Plyr-react. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Plyr-react. Built with Docusaurus.`
       },
       prism: {
         theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+        darkTheme: darkCodeTheme
+      }
+    })
 };
 
 module.exports = config;
